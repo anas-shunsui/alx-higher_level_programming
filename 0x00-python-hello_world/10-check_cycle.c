a78967d771ac65f784428bd4d717342e8fb9e59d
anas-shunsui <alas.anass1@gmail.com>
2023-11-28 00:04:34 +0100

    10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t* bunny = list;
+	listint_t* turtle = list;
+
+	for (; bunny != NULL && turtle != NULL && bunny->next != NULL; )
+	{
+		bunny = bunny->next->next;
+		turtle = turtle->next;
+
+		if (turtle == bunny)
+			return (1);
+	}
+	return (0);
+}
