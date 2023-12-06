aa3aa347d338796c779a7153eda88d4d6f667745
anas-shunsui <alas.anass1@gmail.com>
2023-12-07 00:58:32 +0100

    12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+
+    res = 0
+    oldvalue = 0
+
+    for char in reversed(roman_string):
+        value = dict[char]
+        if value < oldvalue:
+            res -= value
+        else:
+            res += value
+        oldvalue = value
+
+    return (res)
