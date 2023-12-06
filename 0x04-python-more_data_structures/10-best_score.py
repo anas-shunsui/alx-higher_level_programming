ab19d58a9dc59853568aeaa936fd1ebe161c7236
anas-shunsui <alas.anass1@gmail.com>
2023-12-07 00:33:21 +0100

    11. Multiply by using map

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    maxvalue = 0
-    best_key = None
+    max = 0
+    score = None
     if a_dictionary is not None:
         for key, value in a_dictionary.items():
-            if value > maxvalue:
-                maxvalue = value
-                best_key = key
-        return (best_key)
+            if value > max:
+                max = value
+                score = key
+        return (score)
     else:
         return (None)
