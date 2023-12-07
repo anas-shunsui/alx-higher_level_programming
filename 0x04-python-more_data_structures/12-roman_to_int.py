1bf93006242d6fdc9f1afd3c0167a389c07a1572
anas-shunsui <alas.anass1@gmail.com>
2023-12-07 01:07:08 +0100

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,12 @@
 def roman_to_int(roman_string):
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
             'C': 100, 'D': 500, 'M': 1000}
-
+    '''if roman_string is not str or None:
+        return (0)
+    if roman_string is not None:
+        return (0)'''
     res = 0
     oldvalue = 0
-
     for char in reversed(roman_string):
         value = dict[char]
         if value < oldvalue:
